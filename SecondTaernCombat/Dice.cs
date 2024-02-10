c206890ed0e982394f8b1b07bfc8982e4975dd90
Jozef-Piwowarski <jozef.piwowarski2@gmail.com>
2024-02-10 22:41:08 +0100

    Unicorn shit

--- SecondTaernCombat/Dice.cs
+++ SecondTaernCombat/Dice.cs
@@ -10,6 +10,7 @@ namespace SecondTaernCombat
     {
         public static decimal Roll(int sides)
         {
+            //This code is so pog omg so me bestieeeeeee slay
             if (sides <= 0) { return 0; } 
             var rand = new Random();
             return Convert.ToDecimal(rand.Next(1, sides+1));
